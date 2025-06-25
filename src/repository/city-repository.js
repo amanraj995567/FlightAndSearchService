9fcc7d4a0020f63b808d8c4d171c15a044131586
Aman Raj <amanraj700482@gmail.com>
2025-06-25 19:43:05 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,29 +3,65 @@ const {City} = require('../models/index');
 
 class CityRepository{
 
+    /////////////////////
     async createCity({name}){
         try{
             const city = await City.create({name});
+            return city;
 
         }
         catch(error){
+            console.log("Something went wrong in repository layer");
             throw {error};
         }
     }
 
+    /////////////////////////
     async deleteCity({Cityid}){
         try{
            await City.destroy({
                where:{
                    id : Cityid
                }
-           })
+           });
+           return true;
         }
-        catch{error}{
+        catch(error){
+            console.log("Something went wrong in repository layer");
             throw {error}
         }
     }
-}
 
+
+    /////////////////
+
+    async updateCity(cityId,data){
+        try{
+            const city = await City.updateCity(data,{
+                where:{
+                    id : cityId
+                }
+            });
+            return city;
+        }
+        catch(error){
+            console.log("Something went wrong in repository layer");
+            throw {error}
+        }
+    }
+
+
+    ////////////////////////////
+    async getCity(cityId){
+        try{
+           const city = await City.findOne(cityId);
+           return city;
+        }
+        catch(error){
+             console.log("Something went wrong in repository layer");
+            throw {error};
+        }
+    }
+}
 
 module.exports = CityRepository;
